3c17a41080b4ce8fc9e133816760992a46d300fb
Manisha <66690707+M-anisha-coder@users.noreply.github.com>
2022-07-13 20:05:03 +0530

    Add files via upload

---
+++ copy_to_clipboard/script.js
@@ -0,0 +1,22 @@
+
+
+
+//using javascript
+function myFuntion(){
+    var text1=document.getElementById("inputuser");
+    text1.select();
+    text1.setSelectionRange(0,99999);
+    navigator.clipboard.writeText(text1.value); 
+}
+
+
+
+
+//using jQuery
+// $(document).ready(function(){
+//     $('#btn').click(function(){
+//         $('#inputuser').select()
+//         document.execCommand("copy");
+//     });
+
+// });
