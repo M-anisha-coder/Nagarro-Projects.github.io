d7833b2698901644485f5f660f7906261c236e89
Manisha <66690707+M-anisha-coder@users.noreply.github.com>
2022-07-24 10:48:14 +0530

    Update script.js

--- movie_api/script.js
+++ movie_api/script.js
@@ -1,25 +1,27 @@
 const searching = document.getElementById('btn');
 
 function getmovie(){
-    var movieName=document.getElementById('search').value;
-      var yourkey="6607338b" ;
-      const url ='http://www.omdbapi.com/?apikey=[yourkey]& ';
+    var movieName = document.getElementById('search').value;
+    const url =`http://www.omdbapi.com/?i=${movieName}&apikey=6607338b`;
     fetch(url)
     .then(res=>res.json())
     .then(data=>{
-         document.getElementById('result').innerHTML =
-
-        <h1>${data.Title}</h1>,
-        <span>${data.Year}</span>,
-        <h4> Release date ${data.Released}</h4>,
-        <h4>Genre: ${data.Genre}</h4>,
-        <h5>Language: ${data.Language}</h5>,
-        <p>${data.Plot}</p>,
-        <h5>${data.Ratings}</h5> 
-       
-        
+         document.getElementById('result').innerHTML = `
+         <h1>${data.Title}</h1>
+         <span>${data.Year}</span>
+         <h4> Release date ${data.Released}</h4>
+         <h4>Genre: ${data.Genre}</h4>
+         <h5>Language: ${data.Language}</h5>
+         <p>${data.Plot}</p>
+         <h5>${data.Ratings}</h5> 
+         `    
     })
-.catch(err=>alert("movie not found"))
+.catch(err=>{
+    alert("movie not found")
+    document.getElementById('result').style.visibility="hidden";
+    document.getElementById('main').style.visibility="visible";
+})
+
   
 }
 
