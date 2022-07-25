7d9b48a04142cb30454bbaf98f8e7ab6f7cc8c75
Manisha <66690707+M-anisha-coder@users.noreply.github.com>
2022-07-25 23:00:45 +0530

    Add files via upload

---
+++ Theme_changer/script.js
@@ -0,0 +1,30 @@
+function black(){
+    document.getElementById('field').style.backgroundColor="black";
+    document.getElementById('field').style.color="white";
+    document.getElementById('main').style.backgroundColor="black";
+    document.getElementById('field').style.border=" 2px solid white"
+}
+function green(){
+    document.getElementById('field').style.backgroundColor="green";
+    document.getElementById('field').style.color="white";
+    document.getElementById('main').style.backgroundColor="green";
+    document.getElementById('field').style.border=" 2px solid white";
+}
+function yellow(){
+    document.getElementById('field').style.backgroundColor="yellow";
+    document.getElementById('field').style.color="black";
+    document.getElementById('main').style.backgroundColor="yellow";
+    document.getElementById('field').style.border=" 2px solid black";
+}
+function blue(){
+    document.getElementById('field').style.backgroundColor="blue";
+    document.getElementById('field').style.color="black";
+    document.getElementById('main').style.backgroundColor="blue";
+    document.getElementById('field').style.border=" 2px solid black";
+}
+function aqua(){
+    document.getElementById('field').style.backgroundColor="aqua";
+    document.getElementById('field').style.color="black";
+    document.getElementById('main').style.backgroundColor="aqua";
+    document.getElementById('field').style.border=" 2px solid black";
+}
