b55318f9eac9e8bd216449d7e440b470e670f813
Manisha <66690707+M-anisha-coder@users.noreply.github.com>
2022-09-02 21:14:00 +0530

    Delete Twitter_assignment directory

--- Twitter_assignment/routes/profile.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getProfile, getYourProfile } = require('../controllers/profile');
-const router = express.Router();
-
-router.get('/', getYourProfile);
-router.get('/:userId', getProfile);
-
-module.exports = router;
